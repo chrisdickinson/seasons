558256cde046e07283729e033d0ca2699ec77c46
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-06-25 15:12:25 -0700

    add zlib example

--- media/js/main.js
+++ media/js/main.js
@@ -1,21 +1,33 @@
 define(function(require) {
   var d3 = require('d3')
   var seasons = require('./seasons.min')
+    , slider = document.getElementById('range')
+    , pre = document.getElementById('example')
+    , which = document.getElementById('which')
+    , current_data
+    , changes
+    , listener
+    , current
+    
+  slider.addEventListener('change', function(ev) {
+    var new_state = ~~((current_data.length-1) * ~~slider.value / 100)
+    if(new_state !== current) {
+      current = changes(new_state)
+    }
+  }, true)
 
-  got_output(PLATE_DATA)
+  which.addEventListener('change', function(ev) {
+    slider.value = current = 0
+    got_output(window[which.value])
+  })
+
+  got_output(DJANGO_DATA)
 
   function got_output(data) {
-    var changes = seasons('example', data)
-      , current = changes()
-      , slider = document.getElementById('range')
-      
-    slider.addEventListener('change', function(ev) {
-      var new_state = ~~((data.length-1) * ~~slider.value / 100)
-      if(new_state !== current) {
-        current = changes(new_state)
-      }
-    }, true)
-    
+    pre.innerHTML = ''
+    changes = seasons('example', data)
+    current = changes()
+    current_data = data
   }
 })
 
